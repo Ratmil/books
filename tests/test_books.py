cb29eb8630ea19f7e476d40467af383b977af4d5
Ratmil Torres <ratmil@gmail.com>
2022-04-25 11:47:28 -0300

    Fixed tests

--- tests/test_books.py
+++ tests/test_books.py
@@ -4,9 +4,10 @@ from data.openapi import OpenApiStore
 def test_get_book_by_isbn():
     bookStore = OpenApiStore.getInstance()
     book = bookStore.getBookByISBN("9780980200447")    
+    print(book)
     assert book, "One and only one book should have been returned"
-    assert book['ISBN'] == '9780980200447', "Wrong book return"
-    assert book['title'] == 'Slow reading', "Wrong title"    
+    assert book.isbn == '9780980200447', "Wrong book return"
+    assert book.title == 'Slow reading', "Wrong title"    
 
     books = bookStore.getBookByISBN("ffff")
     assert not books, "No book should have been returned"
@@ -16,10 +17,10 @@ def test_get_books_by_isbn():
     bookStore = OpenApiStore.getInstance()
     books = bookStore.getBooksByISBN("9780980200447,0201558025")    
     assert books and len(books) == 2, "One and only one book should have been returned"
-    assert books[0]['ISBN'] == '9780980200447', "Wrong book return"
-    assert books[0]['title'] == 'Slow reading', "Wrong title"   
-    assert books[1]['ISBN'] == '0201558025', "Wrong book return"
-    assert books[1]['title'] == 'Concrete mathematics', "Wrong title"   
+    assert books[0].isbn == '9780980200447', "Wrong book return"
+    assert books[0].title == 'Slow reading', "Wrong title"   
+    assert books[1].isbn == '0201558025', "Wrong book return"
+    assert books[1].title == 'Concrete mathematics', "Wrong title"   
 
 # Test searching books by title
 def test_search_by_title():
@@ -30,4 +31,4 @@ def test_search_by_title():
     assert len(books) > 0 and len(books) <= 10
     search_text = search_text.lower()
     for book in books:
-        assert search_text in book['title'].lower(), "Unmatched return"
+        assert search_text in book.title.lower(), "Unmatched return"
